9c4ab6a0e45879604f6f578e84b8dba75e66af94
Julian Chingal <70417427+Julian-Chingal@users.noreply.github.com>
2026-03-11 23:57:40 -0500

    feat: implement initial DTOs and entity for the Ep module.

--- src/modules/eps/dto/create-ep.dto.ts
+++ src/modules/eps/dto/create-ep.dto.ts
@@ -1,1 +1,58 @@
-export class CreateEpDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsEmail,
+  IsString,
+  IsPhoneNumber,
+  MinLength,
+  IsInt,
+} from 'class-validator';
+
+export class CreateEpDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @ApiProperty()
+  code: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @ApiProperty()
+  taxId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @ApiProperty()
+  legalName: string;
+
+  @IsString()
+  @MinLength(3)
+  @ApiProperty()
+  commercialName: string;
+
+  @IsString()
+  @IsEmail()
+  @ApiProperty()
+  portfolioEmail: string;
+
+  @IsString()
+  @IsPhoneNumber()
+  @ApiProperty()
+  portfolioPhone: string;
+
+  @IsString()
+  @ApiProperty()
+  contactName: string;
+
+  @IsString()
+  @MinLength(4)
+  @ApiProperty()
+  contactPosition: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty()
+  paymentTermDays: number;
+}
--- src/modules/eps/dto/update-ep.dto.ts
+++ src/modules/eps/dto/update-ep.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateEpDto } from './create-ep.dto';
+import { IsBoolean } from 'class-validator';
 
-export class UpdateEpDto extends PartialType(CreateEpDto) {}
+export class UpdateEpDto extends PartialType(CreateEpDto) {
+  @IsBoolean()
+  isActive: boolean;
+}
